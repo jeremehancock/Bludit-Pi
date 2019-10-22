7a7502d02be5806b38aad04ef8689ccf699cd3a8
Jereme <michael@jeremehancock.com>
2019-10-21 19:15:26 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Bludit-Pi
-Setup a Raspberry Pi with Bludit Flat File CMS
+Raspberry Pi Setup for [Bludit Flat File CMS](https://www.bludit.com/)
+
+The goal of this project is to create an easy way to configure and install Bludit on a Raspberry Pi. 
+
+> `This build script should not be used on a Raspberry Pi that has previously had a webserver configured. This script will overwrite the "/var/www/html" directory!`
+
+The following technologies are automatically installed for you:
+* Apache
+* PHP
+* [Bludit v3.10.0 "Mateo"](https://www.bludit.com/)
+
+## Pre-Installation
+1. Raspberry Pi with [Raspbian](https://www.raspberrypi.org/downloads/raspbian/)
+
+## Installation Instructions
+1. Find a directory on your Pi where you'd like to install this repo
+2. Run `git clone git@github.com:mhancoc7/Bludit-Pi.git`
+3. Run `cd Bludit-Pi`
+4. Run `bash build.sh`
+
+## Usage
+1. Make sure the configuration process is completed and your virtual machine is ready
+2. Point your web browser to http://raspberrypi.local to view your Bludit site
+3. Follow the steps to complete the Bludit installation
+
+> #### `Note: All code is provided as-is without any warranty. Use at your own risk.`
---
+++ build.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+# Set to noninteractive
+DEBIAN_FRONTEND=noninteractive
+
+# Update / Upgrade
+sudo -E apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
+
+echo -e "\e[32mUpgrade Complete\e[0m"
+
+# Install apache php
+sudo -E apt-get install -y apache2
+sudo -E apt-get install -y php
+
+echo -e "\e[32mApache/PHP Install Complete\e[0m"
+
+# Install extra php stuff
+sudo -E apt-get install -y php-xml
+sudo -E apt-get install -y php-gd
+sudo -E apt-get install -y php.mbstring
+sudo -E apt-get install -y php-json
+
+echo -e "\e[32mExtra PHP Stuff Install Complete\e[0m"
+
+# Install misc
+sudo -E apt-get install -y vim
+
+echo -e "\e[32mMisc Install Complete\e[0m"
+
+# Set permissions for bludit install
+sudo -E chown -R www-data: /var/www/html
+
+echo -e "\e[32mPermissions Update Complete\e[0m"
+
+# Enable mod_rewrite
+sudo -E a2enmod rewrite
+
+echo -e "\e[32mEnable mod_rewrite Complete\e[0m"
+
+# Update Apache conf
+sudo -E echo '<Directory /var/www/html>' >> /etc/apache2/sites-available/000-default.conf
+sudo -E echo '    Options Indexes FollowSymLinks' >> /etc/apache2/sites-available/000-default.conf
+sudo -E echo '    AllowOverride All' >> /etc/apache2/sites-available/000-default.conf
+sudo -E echo '    Require all granted' >> /etc/apache2/sites-available/000-default.conf
+sudo -E echo '</Directory>' >> /etc/apache2/sites-available/000-default.conf
+
+echo -e "\e[32mUpdated Apache conf Complete\e[0m"
+
+# Chown /var/www/html to www-data:
+sudo chown -R www-data: /var/www/html
+
+echo -e "\e[32mChown Complete\e[0m"
+
+# Copy dashboard to /var/www
+cp -r "localhost/www/html" "/var/www/"
+
+echo -e "\e[32mCopy Bludit Complete\e[0m"
+
+if [ -f "/var/www/html/index.html" ]; then
+  rm /var/www/html/index.html
+fi
+
+echo -e "\e[32mClean-up Complete\e[0m"
+
+# Restart Apache
+sudo -E service apache2 restart
+
+echo -e "\e[32mApache Restart Complete\e[0m"
